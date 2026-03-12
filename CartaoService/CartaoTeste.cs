53eec6784d00c6d501a2176c34ee18ec25072917
Gabriely Rodrigues do Carmo <gabriely.carmo@eaware.io>
2026-03-12 16:28:18 -0300

    Add Tests

---
+++ CartaoService/CartaoTeste.cs
@@ -0,0 +1,57 @@
+﻿using ClienteService.Domain.Entities.Clients;
+using ClienteService.Domain.Entities.Clients.InputCommands;
+using ClienteService.Domain.Entities.Clients.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartaoService
+{
+    public class ClienteTests
+    {
+        [Fact]
+        public void CreateClient_ShouldCreateWithValidData()
+        {
+            var input = new CreatedClientInputCommand
+            {
+                Name = "John Tester",
+                Document = new Document("12345678909"), // CPF válido com dígito correto
+                Email = "john.teste@teste.com.br",
+                Renda = 5000,
+                Score = 700
+            };
+
+            var cliente = Client.Create(input);
+
+            Assert.NotNull(cliente);
+            Assert.Equal(input.Name, cliente.Name);
+            Assert.Equal(input.Document.Value, cliente.Document.Value);
+            Assert.Equal(input.Email, cliente.Email);
+            Assert.Equal(input.Renda, cliente.Renda);
+            Assert.Equal(input.Score, cliente.Score);
+        }
+
+        [Fact]
+        public void CreateDocument_ShouldThrow_WhenDocumentIsEmpty()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Document(string.Empty));
+        }
+
+        [Fact]
+        public void CreateDocument_ShouldThrow_WhenCpfIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Document("11111111111")); // CPF com todos dígitos iguais
+        }
+
+        [Fact]
+        public void CreateDocument_ShouldCleanMask_WhenCpfHasFormatting()
+        {
+            var doc = new Document("123.456.789-09");
+            Assert.Equal("12345678909", doc.Value); // máscara removida
+        }
+    }
+}
---
+++ PropostaService/PropostaTests.cs
@@ -0,0 +1,67 @@
+﻿using PropostaService.Domain.Entities.Propostas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropostaService
+{
+    public class PropostaTests
+    {
+        [Fact]
+        public void CreateProposta_ShouldRefuse_WhenScoreIsLow()
+        {
+            var proposta = Proposta.Create(Guid.NewGuid(), "John Tester", 50);
+            proposta.AnaliseProposta();
+
+            Assert.Equal(0, proposta.MaxCartoes);
+            Assert.Equal(0, proposta.Limite);
+            Assert.Equal(StatusType.Recusada, proposta.Status); // ← adiciona isso
+        }
+
+        [Fact]
+        public void CreateProposta_ShouldApproveOneCard_WhenScoreIsMedium()
+        {
+            var proposta = Proposta.Create(Guid.NewGuid(), "John Tester", 200);
+            proposta.AnaliseProposta();
+
+            Assert.Equal(1, proposta.MaxCartoes);
+            Assert.Equal(1000, proposta.Limite);
+            Assert.Equal(StatusType.Aprovada, proposta.Status); // ← adiciona isso
+        }
+
+        [Fact]
+        public void CreateProposta_ShouldApproveTwoCards_WhenScoreIsHigh()
+        {
+            var proposta = Proposta.Create(Guid.NewGuid(), "John Tester", 700);
+            proposta.AnaliseProposta();
+
+            Assert.Equal(2, proposta.MaxCartoes);
+            Assert.Equal(5000, proposta.Limite);
+            Assert.Equal(StatusType.Aprovada, proposta.Status); // ← adiciona isso
+        }
+
+        [Fact]
+        public void CreateProposta_ShouldRefuse_WhenScoreIsExactly100()
+        {
+            // testa o limite exato da faixa
+            var proposta = Proposta.Create(Guid.NewGuid(), "John Tester", 100);
+            proposta.AnaliseProposta();
+
+            Assert.Equal(0, proposta.MaxCartoes);
+            Assert.Equal(0, proposta.Limite);
+        }
+
+        [Fact]
+        public void CreateProposta_ShouldApproveOneCard_WhenScoreIsExactly101()
+        {
+            // testa o início da segunda faixa
+            var proposta = Proposta.Create(Guid.NewGuid(), "John Tester", 101);
+            proposta.AnaliseProposta();
+
+            Assert.Equal(1, proposta.MaxCartoes);
+            Assert.Equal(1000, proposta.Limite);
+        }
+    }
+}
